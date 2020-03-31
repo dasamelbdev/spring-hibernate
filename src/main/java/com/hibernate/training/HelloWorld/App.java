dfb57930756c30e87a04ae8ee1bf530031e1cb77
git user <dasamelbdev@gmail.com>
2020-03-31 11:22:05 +0530

    adding initial project version

---
+++ src/main/java/com/hibernate/training/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.hibernate.training.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
